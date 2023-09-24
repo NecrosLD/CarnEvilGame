413738747850c6b7ef29793c7f99046ae5ba0afa
CarbonCanine <57195553+CarbonCanine@users.noreply.github.com>
2023-09-24 15:49:16 -0400

    Skiball Launcher Works

---
+++ CarnEvil/Assets/Codes/SkiBall.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkiBall : MonoBehaviour
+{
+    private bool isPressed;
+
+    private float releaseDelay;
+    private float maxDragDistance = 2f;
+
+    private Rigidbody2D rb;
+    private SpringJoint2D sj;
+    private Rigidbody2D slingRb;
+    private LineRenderer lr;
+    private TrailRenderer tr;
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+        sj = GetComponent<SpringJoint2D>();
+        slingRb = sj.connectedBody;
+        lr = GetComponent<LineRenderer>();
+        tr = GetComponent<TrailRenderer>();
+
+        lr.enabled = false;
+        tr.enabled = false;
+
+        releaseDelay = 1 / (sj.frequency * 4);
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (isPressed)
+        {
+            DragBall();
+        }
+    }
+
+    private void DragBall() {
+        SetlineRendererPositions();
+
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float distance = Vector2.Distance(mousePosition, slingRb.position);
+
+
+        if(distance > maxDragDistance) {
+            Vector2 direction = (mousePosition - slingRb.position).normalized;
+            rb.position = slingRb.position + direction * maxDragDistance;
+        } else {
+            rb.position = mousePosition;
+        }
+
+        
+    }
+
+    private void SetlineRendererPositions() {
+        Vector3[] positions = new Vector3[2];
+        positions[0] = rb.position;
+        positions[1] = slingRb.position;
+        lr.SetPositions(positions);
+    }
+
+    private void OnMouseDown()  {
+        isPressed = true;
+        rb.isKinematic = true;
+        lr.enabled = true;
+        tr.enabled = false;
+    }
+
+    private void OnMouseUp() {
+        isPressed = false;
+        rb.isKinematic = false;
+        StartCoroutine(Release());
+        lr.enabled = false;
+        tr.enabled = true;
+    }
+    
+    private IEnumerator Release()
+    {
+        yield return new WaitForSeconds(releaseDelay);
+        sj.enabled = false;
+    }
+
+}
